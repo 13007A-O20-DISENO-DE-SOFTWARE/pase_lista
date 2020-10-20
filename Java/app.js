3dc9dc4eb8b4fcaf48cb00625d4d11685061547f
ElForaneo <alejandro.rios.d.angel@gmail.com>
2020-10-20 04:38:55 -0500

    Avance 2
    Ahora puede hacer un Popup para agregar contenido(PD:Aun no le eh metido las demas opciones, PD2: sigo sin dormir)

---
+++ Java/app.js
@@ -0,0 +1,14 @@
+var agregar = document.getElementById('agregar'),
+    ConPop = document.getElementById('Contenedor-Popup'),
+    Popup = document.getElementById('Popup'),
+    cerrar = document.getElementById('boton-cerrar');
+
+agregar.addEventListener('click', function(){
+    ConPop.classList.add('active');
+    Popup.classList.add('active');
+});
+
+cerrar.addEventListener('click', function(){
+    ConPop.classList.remove('active');
+    Popup.classList.remove('active');
+});
