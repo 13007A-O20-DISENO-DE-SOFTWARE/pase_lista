b37e5b3ec28e7137744e94248cb5b494477e012f
UzielM <uzielmoreno18@gmail.com>
2020-10-21 09:28:55 -0500

    Pequeñas mejoras del diseño, adición de formularios y scripts de js

---
+++ PA_Interactions.js
@@ -0,0 +1,12 @@
+function DeleteP(){
+	var IDP=prompt("Ingrese el ID del profesor a eliminar");
+	if(IDP!=null){
+		var c=confirm("¿Está seguro que desea eliminar al profesor?")
+	}
+}
+function DeleteA(){
+	var IDA=prompt("Ingrese el ID del alumno a eliminar");
+	if(IDA!=null){
+		var c=confirm("¿Está seguro que desea eliminar al alumno?")
+	}
+}
